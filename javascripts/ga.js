37885fc95d298ace4f2aca36c96b7341da3a39c7
Argo Roots <argo@roots.ee>
2015-12-10 10:43:32 +0200

    GA tracking code

--- javascripts/ga.js
+++ javascripts/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'XX-123456-78', 'auto');
+ga('create', 'UA-260765-35', 'auto');
 ga('send', 'pageview');
